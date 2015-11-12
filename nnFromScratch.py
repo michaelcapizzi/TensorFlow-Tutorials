1cca3415fb7797b7459d2d73af92da9ce91b3e61
mcapizzi <mcapizzi@email.arizona>
2015-11-12 12:55:59 -0700

    building methods for building neural networks

--- nnFromScratch.py
+++ nnFromScratch.py
@@ -38,7 +38,7 @@ def perceptron(inputX, w, activationFunction, b=None):
 
 
 #single layer neural network
-def buildMLP1(inputX, w_In, w_Out, activationFunction, b_In=None, b_Out=None):
+def MLP1(inputX, w_In, w_Out, activationFunction, b_In=None, b_Out=None):
     #don't know if the naming and scope is done correctly
         #http://www.tensorflow.org/how_tos/graph_viz/index.md
     with tf.name_scope("hidden") as scope:
@@ -85,7 +85,9 @@ def buildMLP1(inputX, w_In, w_Out, activationFunction, b_In=None, b_Out=None):
 #LOSS
 ###############
 
-def calculateLoss(logits, labels):
+#logits => output of INFERENCE MODEL
+#labels => true labels corresponding to output logits
+def calculateLossOp(logits, labels):
     #calculate cross entropy
     crossEntropy = tf.nn.softmax_cross_entropy_with_logits(logits, labels, name="cross-entropy")
 
@@ -99,7 +101,8 @@ def calculateLoss(logits, labels):
 #TRAINING
 ###############
 
-def training(loss, learningRate):
+#loss => output of LOSS
+def trainingOp(loss, learningRate):
     #scalar summary for snapshot of loss
     tf.scalar_summary(loss.op.name, loss)       #don't know what this does?
 
@@ -116,30 +119,32 @@ def training(loss, learningRate):
 
 
 ###############
+#PREDICT
+###############
+
+def predictOp(inferenceModel):
+    tf.argmax(inferenceModel, 1)
+
+###############
 #EVALUATION
 ###############
 
 #takes same inputs as calculateLoss?
-def evaluate(logits, labels):
+def evaluateOp(logits, labels):
     #calculates how many of logits in top 1 matched label
     correct = tf.nn.in_top_k(logits, labels, 1)
 
     #returns tensor of boolean values
     return correct
 
-
 ########################################################################
 
-#TODO figure out how to run the parts now
-#https://tensorflow.googlesource.com/tensorflow/+/master/tensorflow/g3doc/tutorials/mnist/fully_connected_feed.py
-
-########################################################################
-
+#TODO figure out if I need these
 #set up placeholders
-w_In_P = tf.placeholder("float")
-w_Out_P = tf.placeholder("float")
-b_In_P = tf.placeholder("float")
-b_Out_P = tf.placeholder("float")
+# w_In_P = tf.placeholder("float")
+# w_Out_P = tf.placeholder("float")
+# b_In_P = tf.placeholder("float")
+# b_Out_P = tf.placeholder("float")
 
 #randomly initialize weights
     #how to do with -root(6/d) to root(6/d)
@@ -148,6 +153,31 @@ def initialize_weights(rows, columns):
     # return tf.Variable(tf.random_normal([rows, columns], stddev=0.01))
 
 
-#fill feed_dict
-#TODO build
+########################################################################
 
+#https://tensorflow.googlesource.com/tensorflow/+/master/tensorflow/g3doc/tutorials/mnist/fully_connected_feed.py
+
+#initialize placeholders
+trainX = tf.placeholder("float")        #can add size if needed
+trainY = tf.placeholder("float")        #can add size if needed
+testX = tf.placeholder("float")
+testY = tf.placeholder("float")
+
+#start session
+sess = tf.Session()
+
+#initialize variables
+sess.run(tf.initialize_all_variables())
+
+#run training
+    #iterations => # of iterations of training
+    #data => tuple of (X (data) Y (label))
+    #trainOp => training(calculateLoss([INFERENCE MODEL], [labels])), .01)
+def train(session, iterations, data, trainOp):
+    for i in range(iterations):
+        for d in data:
+            session.run(trainOp, feed_dict={trainX: d[0], trainY: d[1]})
+
+#TODO figure out why we need testY
+def predict(session, inferenceModel, X, Y):
+    session.run(predictOp(inferenceModel), feed_dict={X: testX, Y: testY})
